80fd32b16ba318af7c7d5c2ce03cbce0d4e86fa4
Chidi Orji <orjichidi95@gmail.com>
2021-01-05 04:15:26 +0100

    minor change

--- src/LeftSideBar.tsx
+++ src/LeftSideBar.tsx
@@ -28,6 +28,30 @@ export const LeftSideBar: React.FC = () => {
   }
   `;
 
+  const c = {
+    name: 'Chidi Orji',
+    title: 'Frontend developer',
+    version: 'Living',
+    phone: '09036650603',
+    email: 'orjichidi95@gmail.com',
+    location: 'Lagos, Nigeria',
+    education: 'B.Eng (Petroleum Engineering), University of Benin',
+    social: {
+      homepage: 'https://chidimo.netlify.app',
+      twitter: 'https://www.twitter.com/chi_di_mo',
+      linkedin: 'https://www.linkedin.com/in/chidi-orji/',
+      github: 'https://github.com/chidimo',
+    },
+    languages: {
+      python: 'latest',
+      javascript: 'latest',
+    },
+    frameworks: {
+      react: 'latest',
+      django: 'latest',
+    },
+  };
+
   return (
     <Card className="">
       <pre>{details}</pre>
